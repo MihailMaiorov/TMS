a8c9aaa1d1875040a1c9abc0ec82c9cc30ca4f6d
MihailMaiorov <7351943@gmail.com>
2022-12-13 21:43:27 +0300

    rrr

---
+++ lesson2/practice.rb
@@ -0,0 +1,13 @@
+loop do
+  color = %w[black red green].shuffle
+  puts 'Enter you color (black, green, red)'
+
+  user_color = gets.strip
+
+  if user_color == color[0]
+    puts 'You win!!!'
+  else
+    puts 'You lose!!!'
+    puts "Color is #{color[0]}"
+  end
+end
