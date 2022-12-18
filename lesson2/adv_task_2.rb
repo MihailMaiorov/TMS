afc9bae8e36f383e382d37dadb146bc879f54499
MihailMaiorov <7351943@gmail.com>
2022-12-19 02:48:23 +0300

    Fix roulette after review add CONFIG

--- lesson2/adv_task_2.rb
+++ lesson2/adv_task_2.rb
@@ -1,60 +1,90 @@
-puts "Добро пожаловать в Американскую рулетку! \nВведите свое имя:"
-name = gets.strip
-money = 100.00
-contin_game = %w[y n]
-all_color = %w[red black green]
+MIN = 1
+MAX = 38
+CONFIG = {
+  'red' => { 'coefficient' => 2, 'numbers' => (1..18) },
+  'black' => { 'coefficient' => 2, 'numbers' => (19..36) },
+  'green' => { 'coefficient' => 36, 'numbers' => (37..38) }
+}.freeze
 
-until money.zero?
-  puts "#{name} Ваш стек равен #{money}"
-  puts 'Желаете сделать ставку? (y/n)'
-  game = gets.strip[0]
+ALL_COLORS = %w[red black green].freeze
+CONTINUATION_GAME = %w[y n].freeze
 
-  until contin_game.include?(game)
-    puts 'Некорректный ответ. Введите (y/n)'
-    game = gets.strip[0]
-  end
+def color_drop
+  random_number_color = rand(MIN..MAX)
 
-  if game == 'y'
+  CONFIG.select { |key, value| return key if value['numbers'].include?(random_number_color) }
+end
 
-    rand_color = rand(1..38)
+def user_input(welcome_msg: nil, validator: nil, error_msg: nil)
+  loop do
+    puts welcome_msg unless welcome_msg.nil?
 
-    if rand_color.between?(1, 18)
-      color = 'black'
-    elsif rand_color.between?(19, 36)
-      color = 'red'
-    else
-      color = 'green'
-    end
+    value = gets.strip
 
-    puts 'Выберите цвет: (black, red, green)'
-    user_color = gets.strip
+    return value if validator.nil? || validator.call(value)
 
-    until all_color.include?(user_color)
-      puts 'Некорректный цвет. (black, red, green)'
-      user_color = gets.strip
-    end
-
-    puts 'Сделайте ставку'
-    bet = gets.to_f
-
-    while bet > money
-      puts "Недостаточно средств (стек равен #{money})"
-      bet = gets.to_f
-    end
-
-    money -= bet
-    puts "Ставки сделаны, ставок больше нет!\nВыпал: #{color}"
-    color == 'green' ? value = bet * 36 : value = bet * 2
-
-    if user_color == color
-      puts "Вы выиграли #{value}!!!"
-      money += value
-    else
-      puts 'Вы проиграли.'
-    end
-
-  else
-    puts "Спасибо за игру. Ваш стек равен #{money}"
-    break
+    puts error_msg unless error_msg.nil?
   end
 end
+
+def continuation_game
+  user_input(welcome_msg: 'Желаете сделать ставку? (y/n)',
+             validator: ->(value) { CONTINUATION_GAME.include?(value) },
+             error_msg: 'Некорректный ответ.')
+end
+
+def player_name
+  user_input(welcome_msg: 'Ваше имя:',
+             validator: ->(value) { !value.empty? },
+             error_msg: 'Имя не может быть пустым!')
+end
+
+def player_choose_color
+  user_input(welcome_msg: 'Выберите цвет: (red, black, green)',
+             validator: ->(value) { ALL_COLORS.include?(value) },
+             error_msg: 'Некорректный цвет.')
+end
+
+def player_bet(money)
+  user_input(welcome_msg: "Сделайте ставку. (стек равен #{money})",
+             validator: ->(value) { value.to_i <= money && value.to_i.positive? },
+             error_msg: 'Некорректная ставка.')
+end
+
+def game_result(player_color, color, bet_value, bet)
+  if player_color == color
+    puts "Вы выиграли #{bet_value}!!!"
+    bet_value - bet
+  else
+    puts "Вы проиграли #{bet}"
+    -bet
+  end
+end
+
+def game_runer
+  puts 'Добро пожаловать в Американскую рулетку!'
+  name = player_name
+  money = 100.00
+
+  while money.positive?
+    puts "#{name} Ваш стек равен #{money}"
+    break unless continuation_game == 'y'
+
+    player_color = player_choose_color
+    puts "Вы выбрали: #{player_color}"
+
+    bet = player_bet(money).to_i
+
+    puts "Ваша ставка: #{bet}"
+
+    color = color_drop
+
+    bet_value = bet * CONFIG[color]['coefficient']
+
+    puts "Ставки сделаны, ставок больше нет!\nВыпал: #{color}"
+
+    money += game_result(player_color, color, bet_value, bet)
+  end
+end
+
+game_runer
--- lesson2/task_2.rb
+++ lesson2/task_2.rb
@@ -1,9 +1,18 @@
+# # Идеальный вес. Программа запрашивает у пользователя имя и рост
+#  и выводит идеальный вес по формуле (<рост> - 110) * 1.15,
+#  после чего выводит результат пользователю на экран с обращением по имени.
+#  Если идеальный вес получается отрицательным, то выводится строка "Ваш вес уже оптимальный"
+
 loop do
   puts 'Enter your name.'
   name = gets.strip
   puts 'Enter your growth.'
   growth = gets.to_f
   ideal_weight = (growth - 110) * 1.15
-  puts "#{name} your ideal weight is #{ideal_weight}."
-  puts 'Your weight is optimal.' if ideal_weight.negative?
+
+  if ideal_weight.positive?
+    puts "#{name} your ideal weight is #{ideal_weight}."
+  else
+    puts 'Your weight is optimal.'
+  end
 end
