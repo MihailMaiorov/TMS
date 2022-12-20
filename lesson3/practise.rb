3871f512b0a7ba2488b4daebbfc1cce4ff4c9e22
MihailMaiorov <7351943@gmail.com>
2022-12-20 23:54:47 +0300

    Fix after review, removed unnecessary

--- lesson3/practise.rb
+++ lesson3/practise.rb
@@ -1,42 +1,24 @@
 # +1. компьютер загадывает число
-# 2. просим пользователя угадать число (рассказать пользователю про правила игры)
-# 3. проверяем угадал ли и чуть что подсказываем
-# 4. обновляем количество попыток
+# 2. компьютер отгадывает число
 
 MIN = 0
-MAX = 100_000_000_000_000_000_000_000_000
+MAX = 100_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000
 MAX_TRIES = 7
-INPUT_EQUEL = 'Вы выиграли!'.freeze
-INPUT_LOWER = 'Ваше число меньше загаданного!'.freeze
-INPUT_HIGER = 'Ваше число больше загаданного!'.freeze
-
-def right_guess?(users_guess, secret_number)
-  if users_guess == secret_number
-    INPUT_EQUEL
-  elsif users_guess < secret_number
-    INPUT_LOWER
-  elsif users_guess > secret_number
-    INPUT_HIGER
-  end
-end
 
 secret_number = rand(MIN..MAX)
-current_guess = (MIN + MAX) / 2
 lower_bound = MIN
 upper_bound = MAX
+computer_guess = (lower_bound + upper_bound) / 2
 
 loop do
-  guess_result = right_guess?(current_guess, secret_number)
-
-  case guess_result
-  when INPUT_EQUEL
+  if computer_guess < secret_number
+    lower_bound = computer_guess
+    computer_guess += ((upper_bound - lower_bound) / 2.0).round
+  elsif computer_guess > secret_number
+    upper_bound = computer_guess
+    computer_guess -= ((upper_bound - lower_bound) / 2.0).round
+  else
     puts "Победа! #{secret_number}"
     break
-  when INPUT_LOWER
-    lower_bound = current_guess
-    current_guess += ((upper_bound - current_guess) / 2.0).round
-  when INPUT_HIGER
-    upper_bound = current_guess
-    current_guess -= ((current_guess - lower_bound) / 2.0).round
   end
 end
