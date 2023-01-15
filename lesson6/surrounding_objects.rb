973458847250de734d0635cc4bff15fc16c78cfe
MihailMaiorov <7351943@gmail.com>
2023-01-15 14:47:08 +0300

    add surrounding_objects

--- lesson6/surrounding_objects.rb
+++ lesson6/surrounding_objects.rb
@@ -1,8 +1,36 @@
 # Взять несколько предметов, которые вас окружают, и описать их, как классы.
 # Создать по объекту каждого класса, сравнить их поведение.
 
-class Laptop
-  def initialize(title)
-    @title = title
+class Smartphone
+  attr_reader :brand, :model
+
+  def initialize(brand, model)
+    @brand = brand
+    @model = model
+  end
+
+  def info
+    puts "My phone is #{brand}: #{model}"
+  end
+
+  def make_call(contact)
+    puts "Calling a #{contact}."
+  end
+
+  def end_the_call
+    puts 'Сall ended.'
+  end
+
+  def send_message(contact, text)
+    puts "Message: '#{text}' sent to '#{contact}'."
+  end
+
+  def system_update
+    puts 'System updated to latest version'
   end
 end
+
+iphone = Smartphone.new('Apple', 'Iphone X')
+iphone.info
+iphone.make_call('Mike')
+iphone.send_message('Mike', 'Hi bro!')
