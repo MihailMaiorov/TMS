ae7fe0ce9a24afa242dc84d1078215ad26758313
MihailMaiorov <7351943@gmail.com>
2023-01-25 23:59:55 +0300

    add task 1

---
+++ lesson10/number_checker.rb
@@ -0,0 +1,24 @@
+# Написать программу, которая проверяет что подаётся на ввод.
+# Если подаётся что-то вместо числа - вызывать исключение и в обработке вывести, что было подано.
+
+class NumberChecker
+  MAX_RETRY = 5
+
+  def check
+    retry_count = 0
+
+    begin
+      print 'Enter a number: '
+      number = Integer(gets.strip)
+      puts "#{number} is the number!"
+    rescue ArgumentError => e
+      puts e
+      retry_count += 1
+      retry if retry_count < MAX_RETRY
+      puts 'Gotta learn the numbers!!!'
+    end
+  end
+end
+
+a = NumberChecker.new
+a.check
