21561018f89e08aa0a9430cb65424b00ff8e2218
MihailMaiorov <7351943@gmail.com>
2023-01-31 20:21:44 +0300

    lesson11 remade task1

--- lesson11/task1.rb
+++ lesson11/task1.rb
@@ -1,7 +1,7 @@
 class MyValididator
   LOGIN_MAX_SIZE = 19
   LOGIN_VALIDATE = /\A\w{1,19}\z/.freeze
-  RASSWORD_MAX_SIZE = 19
+  PASSWORD_MAX_SIZE = 19
   PASSWORD_VALIDATE = /\A\w{1,19}\z/.freeze
 
   attr_reader :login
@@ -17,65 +17,66 @@ class MyValididator
   attr_reader :password, :confirmed_password
 
   def check_login
-    LoginChecker.check(welcome_msg: 'Enter your login: ',
-                       validator: ->(value) { value.match?(LOGIN_VALIDATE) })
+    LoginChecker.new.check(welcome_msg: 'Enter your login: ',
+                           validator: ->(value) { value.match?(LOGIN_VALIDATE) })
   end
 
   def check_password
-    PasswordChecker.check(welcome_msg: 'Enter your password: ',
-                          validator: ->(value) { value.match?(PASSWORD_VALIDATE) })
+    PasswordChecker.new.check(welcome_msg: 'Enter your password: ',
+                              validator: ->(value) { value.match?(PASSWORD_VALIDATE) })
   end
 
   def confirm_password
-    PasswordChecker.confirm(welcome_msg: 'Confirm your password: ',
-                            validator: ->(value) { value == password })
+    PasswordChecker.new.confirm(welcome_msg: 'Confirm your password: ',
+                                validator: ->(value) { value == password })
+  end
+end
+
+class LoginChecker
+  def check(welcome_msg:, validator:)
+    print welcome_msg
+    value = gets.strip
+    raise self.class::WrongLoginException.valid_error unless validator.call(value)
+
+    value
   end
 
   class WrongLoginException < StandardError
     def self.valid_error
-      "only latin letters, numbers and underscores, max size #{MyValididator::LOGIN_MAX_SIZE}"
+      msg = "only latin letters, numbers and underscores, max size #{MyValididator::LOGIN_MAX_SIZE}"
+      new(msg)
     end
+  end
+end
+
+class PasswordChecker
+  def check(welcome_msg:, validator:)
+    print welcome_msg
+    value = gets.strip
+    raise self.class::WrongPasswordException.valid_error unless validator.call(value)
+
+    value
+  end
+
+  def confirm(welcome_msg:, validator:)
+    print welcome_msg
+    value = gets.strip
+    raise self.class::WrongPasswordException.confirm_error unless validator.call(value)
+
+    value
   end
 
   class WrongPasswordException < StandardError
     class << self
       def valid_error
-        "only latin letters, numbers and underscores, max size #{MyValididator::RASSWORD_MAX_SIZE}"
+        msg = "only latin letters, numbers and underscores, max size #{MyValididator::PASSWORD_MAX_SIZE}"
+        new(msg)
       end
 
       def confirm_error
-        'password and confirm password must be equal'
+        msg = 'password and confirm password must be equal'
+        new(msg)
       end
-    end
-  end
-end
-
-class LoginChecker
-  def self.check(welcome_msg:, validator:)
-    print welcome_msg
-    value = gets.strip
-    raise MyValididator::WrongLoginException.valid_error unless validator.call(value)
-
-    value
-  end
-end
-
-class PasswordChecker
-  class << self
-    def check(welcome_msg:, validator:)
-      print welcome_msg
-      value = gets.strip
-      raise MyValididator::WrongPasswordException.valid_error unless validator.call(value)
-
-      value
-    end
-
-    def confirm(welcome_msg:, validator:)
-      print welcome_msg
-      value = gets.strip
-      raise MyValididator::WrongPasswordException.confirm_error unless validator.call(value)
-
-      value
     end
   end
 end
