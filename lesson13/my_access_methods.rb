501dca44a98f918f39df0800ce11835503e95d0b
MihailMaiorov <7351943@gmail.com>
2023-02-07 00:39:24 +0300

    Homework lesson13 add access_methods

---
+++ lesson13/my_access_methods.rb
@@ -0,0 +1,40 @@
+module MyAccessMethods
+  def my_attr_reader(*args)
+    args.each do |name|
+      define_method(name) do
+        instance_variable_get("@#{name}")
+      end
+    end
+  end
+
+  def my_attr_writer(*args)
+    args.each do |name|
+      define_method("#{name}=") do |value|
+        instance_variable_set("@#{name}", value)
+      end
+    end
+  end
+
+  def my_attr_accessor(*args)
+    args.each do |name|
+      define_method(name) do
+        instance_variable_get("@#{name}")
+      end
+
+      define_method("#{name}=") do |value|
+        instance_variable_set("@#{name}", value)
+      end
+    end
+  end
+end
+
+class TestMyAccessMethods
+  extend MyAccessMethods
+
+  my_attr_accessor :name, :age
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+end
